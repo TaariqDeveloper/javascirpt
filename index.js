c02bd111f9ba8d87233e78a3e9a8507a409a09ca
TaariqDeveloper <TaariqDeveloper@gmail.com>
2024-11-30 19:03:34 +0300

    java

---
+++ index.js
@@ -0,0 +1,63 @@
+// let x = 9;
+// x = 66;
+// console.log(x);
+
+// let age =20;
+// let userName ="mohamed";
+// let today =true;
+// var num = 2.6
+// console.log(age, userName, today,num);
+
+
+// let addition = 4 +5;
+// console.log(addition);
+// let sub = 8-4;
+// console.log(sub);
+// console.log("Adition", (5+8));
+// console.log("substration", (8-4));
+// console.log("Muliplexation", (10*10));
+// console.log("divistioj", (8/4));
+// console.log("expo", (10**2));
+// console.log("haraa", (10%3));
+
+// let num4 =20;
+// num4-=3;
+// console.log("incriment: " ,num4)
+
+// let num5 = 10;
+// num5 -=3;
+// console.log("decriment: " , num5)
+// let numbers1 =20;
+// let number2=40;
+
+
+//user prompt
+// assingmet
+// name 
+// adress
+// phone number
+// birthdate
+
+let FullName = prompt("enter your full name")
+let Adress = prompt("enter your Adress")
+let PhoneNumber = parseInt(prompt("enter your phone number"))
+let birthdate = parseInt(prompt("enter your birthdate"))
+let Age = [2024 - birthdate]
+console.log("Full Name: " + FullName);
+console.log("Adress: " + Adress);
+console.log("Phone Number: " + PhoneNumber);
+console.log("Birthdate: " + Age);
+
+
+
+
+// let result= numbers1 + number2
+// alert("result: " + result)
+
+// let name  = prompt("enter your name")
+// console.log("name: " + name);
+// let num1 = parseInt(prompt("enter number 1"))
+// let num2 = parseInt(prompt("enter number 2"))
+// console.log("sum fo num1 and num2 is  " , num1 + num2)
+
+
